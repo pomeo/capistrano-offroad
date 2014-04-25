dee6ae5351c5c6b41ef3376a7047cc7d4ba718b8
Sergey Ovechkin <me@sovechkin.com>
2014-04-25 12:27:21 +0400

    add more defaults

--- lib/capistrano-offroad/modules/defaults.rb
+++ lib/capistrano-offroad/modules/defaults.rb
@@ -2,8 +2,23 @@
 require 'capistrano'
 
 Capistrano::Configuration.instance(:must_exist).load do
+  default_run_options[:pty] = true
+  default_run_options[:shell] = '/bin/bash'
   set :scm, :git
+  set :deploy_via,  :copy
+  set :user,        "pomeo"
+  set :port,        2222
+  set :copy_exclude,["/.git/", "/.gitignore", "/Capfile", "/config/", "/config.yaml", "/Rakefile", "Rules", "/tmp/", "/mkmf.log"]
   set :ssh_options, { :forward_agent => true }
   set :use_sudo, false
-  set :deploy_to, "/srv/#{application}"
+  set :deploy_to, "/home/ubuntu/www"
+  set :deploy_user, "ubuntu"
+  set :deploy_group,"ubuntu"
+  
+  namespace :deploy do 
+    desc "Install node modules non-globally"
+    task :npm_install do
+      run ". /home/ubuntu/.nvm/nvm.sh && cd #{current_path} && npm install"
+    end
+  end
 end
