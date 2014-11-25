6611002e917e0370f19c1cf0178249ca2e7a48e8
Sergey Ovechkin <me@sovechkin.com>
2014-11-25 17:09:55 +0300

    change user

--- lib/capistrano-offroad/modules/defaults.rb
+++ lib/capistrano-offroad/modules/defaults.rb
@@ -6,7 +6,7 @@ Capistrano::Configuration.instance(:must_exist).load do
   default_run_options[:shell] = '/bin/bash'
   set :scm, :git
   set :deploy_via,  :copy
-  set :user,        "pomeo"
+  set :user,        "ubuntu"
   set :port,        2222
   set :copy_exclude,["/.git/", "/.gitignore", "/Capfile", "/config/", "/config.yaml", "/Rakefile", "Rules", "/tmp/", "/mkmf.log"]
   set :ssh_options, { :forward_agent => true }
@@ -14,8 +14,8 @@ Capistrano::Configuration.instance(:must_exist).load do
   set :deploy_to, "/home/ubuntu/www"
   set :deploy_user, "ubuntu"
   set :deploy_group,"ubuntu"
-  
-  namespace :deploy do 
+
+  namespace :deploy do
     desc "Install node modules non-globally"
     task :npm_install do
       run ". /home/ubuntu/.nvm/nvm.sh && cd #{current_path} && npm install"
