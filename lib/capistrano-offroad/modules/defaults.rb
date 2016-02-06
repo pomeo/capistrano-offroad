6b2df70654ff8b8bd16c826194add8514826dfb5
Sergey Ovechkin <me@pomeo.me>
2016-02-06 20:09:34 +0300

    remove config dir from exclude

--- lib/capistrano-offroad/modules/defaults.rb
+++ lib/capistrano-offroad/modules/defaults.rb
@@ -8,7 +8,7 @@ Capistrano::Configuration.instance(:must_exist).load do
   set :deploy_via,  :copy
   set :user,        "ubuntu"
   set :port,        2222
-  set :copy_exclude,["/.git/", "/.gitignore", "/Capfile", "/config/", "/config.yaml", "/Rakefile", "Rules", "/tmp/", "/mkmf.log"]
+  set :copy_exclude,["/.git/", "/.gitignore", "/Capfile", "/config.yaml", "/Rakefile", "Rules", "/tmp/", "/mkmf.log"]
   set :ssh_options, { :forward_agent => true }
   set :use_sudo, false
   set :deploy_to, "/home/ubuntu/www"
